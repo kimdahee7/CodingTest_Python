a413cc2b18b189242ba365c59858d4c7a8d11aa4
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2023-05-26 13:18:01 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,14 @@
+def main():
+  x = int(input())
+  if x >= 90:
+    print('A')
+  elif x >= 80:
+    print('B')
+  elif x >= 70:
+    print('C')
+  elif x >= 60:
+    print('D')
+  else:
+    print('F')
+    
+main()
