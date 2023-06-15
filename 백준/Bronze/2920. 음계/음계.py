f021bd2b13c3d05072850f056df0467d9e517f65
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2023-06-15 19:52:30 +0900

    [Bronze II] Title: 음계, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,10 @@
+def main():
+  x = list(map(int,input().split()))
+  if x == sorted(x):
+    print("ascending")
+  elif x == sorted(x,reverse=True):
+    print("descending")
+  else:
+    print("mixed")
+  
+main()
