7ac335e644d80bd03162c080e3da6b962d6e2f33
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2023-07-26 19:20:18 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -3,62 +3,59 @@ input = sys.stdin.readline
 def main():
   L = int(input())
   l = list(map(str,input().strip()))
-  print(hashing(l))
-
-def hashing(l):
   sum = 0
   for i in range(len(l)):
     if l[i] == "a":
-      sum += 1*(31**i)
+      sum += 1*pow(31,i)
     if l[i] == "b":
-      sum += 2*(31**i)
+      sum += 2*pow(31,i)
     if l[i] == "c":
-      sum += 3*(31**i)
+      sum += 3*pow(31,i)
     if l[i] == "d":
-      sum += 4*(31**i)
+      sum += 4*pow(31,i)
     if l[i] == "e":
-      sum += 5*(31**i)
+      sum += 5*pow(31,i)
     if l[i] == "f":
-      sum += 6*(31**i)
+      sum += 6*pow(31,i)
     if l[i] == "g":
-      sum += 7*(31**i)
+      sum += 7*pow(31,i)
     if l[i] == "h":
-      sum += 8*(31**i)
+      sum += 8*pow(31,i)
     if l[i] == "i":
-      sum += 9*(31**i)
+      sum += 9*pow(31,i)
     if l[i] == "j":
-      sum += 10*(31**i)
+      sum += 10*pow(31,i)
     if l[i] == "k":
-      sum += 11*(31**i)
+      sum += 11*pow(31,i)
     if l[i] == "l":
-      sum += 12*(31**i)
+      sum += 12*pow(31,i)
     if l[i] == "m":
-      sum += 13*(31**i)
+      sum += 13*pow(31,i)
     if l[i] == "n":
-      sum += 14*(31**i)
+      sum += 14*pow(31,i)
     if l[i] == "o":
-      sum += 15*(31**i)
+      sum += 15*pow(31,i)
     if l[i] == "p":
-      sum += 16*(31**i)
+      sum += 16*pow(31,i)
     if l[i] == "q":
-      sum += 17*(31**i)
+      sum += 17*pow(31,i)
     if l[i] == "r":
-      sum += 18*(31**i)
+      sum += 18*pow(31,i)
     if l[i] == "s":
-      sum += 19*(31**i)
+      sum += 19*pow(31,i)
     if l[i] == "t":
-      sum += 20*(31**i)
+      sum += 20*pow(31,i)
     if l[i] == "u":
-      sum += 21*(31**i)
+      sum += 21*pow(31,i)
     if l[i] == "v":
-      sum += 22*(31**i)
+      sum += 22*pow(31,i)
     if l[i] == "w":
-      sum += 23*(31**i)
+      sum += 23*pow(31,i)
     if l[i] == "x":
-      sum += 24*(31**i)
+      sum += 24*pow(31,i)
     if l[i] == "y":
-      sum += 25*(31**i)
+      sum += 25*pow(31,i)
     if l[i] == "z":
-      sum += 26*(31**i)
-  return sum 
+      sum += 26*pow(31,i)
+  print(sum) 
 main()
