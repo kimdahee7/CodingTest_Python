c914c7b838a2d57e566deceb9fa268b795095fea
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2023-08-10 14:51:45 +0900

    [Silver II] Title: 최소 힙, Time: 240 ms, Memory: 40668 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+from queue import PriorityQueue
+def main():
+  l = PriorityQueue()
+  N = int(input())
+  for _ in range(N):
+    x = int(input())
+    if x != 0:
+      l.put(x)
+    else:
+      if l.empty():
+        print(0)
+      else:
+        print(l.get())
+main()
