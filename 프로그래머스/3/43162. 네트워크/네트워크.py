7b1fc881b11b471bc4b4812c2bb968ac1529077a
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2023-10-18 18:44:09 +0900

    [level 3] Title: 네트워크, Time: 0.45 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,20 @@
+def solution(n, computers):
+    answer = 0
+    graph = [[] for _ in range(n)]
+    for i in range(len(computers)):
+        for j in range(len(computers[i])):
+            if computers[i][j] == 1:
+                graph[i].append(j)
+    visited = [False] * (n)
+    for i in range(n):
+        if visited[i] == False:
+            dfs(graph,i,visited)
+            answer+=1
+    return answer
+
+def dfs (graph, n, visited):
+    visited[n] = True
+    for i in graph[n]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+    
