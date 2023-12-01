d010a80b3de581f2c2a531a42b2549ddf4a2cea9
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2023-12-01 23:17:04 +0900

    [Silver I] Title: 정수 삼각형, Time: 160 ms, Memory: 35612 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,11 @@
+N = int(input())
+dp= [list(map(int,input().split())) for _ in range(N)]
+for i in range(1,N):
+  for j in range(len(dp[i])):
+    if j == 0:
+      dp[i][j] += dp[i-1][j]
+    elif j == len(dp[i])-1:
+      dp[i][j] += dp[i-1][j-1]
+    else:
+      dp[i][j] += max(dp[i-1][j-1],dp[i-1][j])
+print(max(dp[N-1]))
