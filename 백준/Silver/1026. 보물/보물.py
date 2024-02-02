bfd54c83015b7a2f26647760fa5129f4cbd59cb3
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-02-02 22:16:15 +0900

    [Silver IV] Title: 보물, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,9 @@
+N = int(input())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+A.sort()
+B.sort(reverse=True)
+total = 0
+for i in range(len(A)):
+  total += A[i]*B[i]
+print(total)
