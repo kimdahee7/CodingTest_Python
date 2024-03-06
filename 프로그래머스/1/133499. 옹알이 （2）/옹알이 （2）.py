8e18c79a24f6f54ab339b4ddb828ae44ad8d542c
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-03-06 20:19:13 +0900

    [level 1] Title: 옹알이 (2), Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,25 +1,21 @@
 from itertools import product
 def solution(babbling):
+    answer = 0
     l = ["aya","ye","woo","ma"]
     exp = []
     new_l = []
     for i in l:
         exp.append(i*2)
     for i in babbling:
-        c = 0 
+        c = 0
         for j in exp:
             if j in i:
-                c += 1
+                c +=1
                 break
-        if c  == 0:
-            new_l.append(i)
-    answer = len(new_l)
-    for i in new_l:
-        for j in l:
-            if j in i:
-                i = i.replace(j,"0")
-        for a in i:
-            if a != "0":
-                answer -=1
-                break
+        if c == 0:
+            for j in l:
+                if j in i:
+                    i = i.replace(j," ")
+            if len(i.strip()) == 0:
+                answer +=1
     return answer
