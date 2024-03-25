7b4569f08557d6c6afb62e4c9c8cf1e74255f34a
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-03-25 23:27:04 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,12 @@
 def solution(clothes):
+    answer = 1
     dic = {}
     for i in clothes:
         if i[1] in dic:
             dic[i[1]] +=1
         else:
             dic[i[1]] = 1
-    total = 1
-    for a in dic.keys():
-        total *= dic[a] + 1
-    total -=1
-    return total
+    for i in dic:
+        answer *= dic[i] +1
+    return answer-1
+
