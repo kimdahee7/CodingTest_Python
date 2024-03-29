a917ebbcb8def2dc93fe904300601655abee0a4c
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-03-29 22:43:47 +0900

    [level 2] Title: 전화번호 목록, Time: 96.40 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,8 @@
 def solution(phone_book):
     phone_book.sort()
-    for i in range(len(phone_book)-1):
-        if phone_book[i] in phone_book[i+1][:len(phone_book[i])]:
-                return False
+    n = len(phone_book)
+    for i in range(n-1):
+        if phone_book[i] == phone_book[i+1][0:len(phone_book[i])]:
+            return False
+            exit()
     return True
