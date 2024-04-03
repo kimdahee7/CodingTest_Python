8978a87c0c0edb5e598b507ea19d766bda26fbdd
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-04-03 17:38:38 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,19 +1,16 @@
+from collections import deque
 def solution(priorities, location):
-    index = [i for i in range(len(priorities))]
-    total = 1
-    while len(index) > 0:
-        max_data = max(priorities)
-        if priorities[0] == max_data:
-            if index[0] == location:
-                answer = total
-                break
-            else:
-                priorities.pop(0)
-                index.pop(0)
-                total +=1
-        else:
-            priorities.append(priorities[0])
+    answer = 0
+    l = deque()
+    for i in range(len(priorities)):
+        l.append((i,priorities[i])) 
+    priorities.sort(reverse=True)
+    while True:
+        if l[0][1] == priorities[0]:
+            answer +=1
+            if l[0][0] == location:
+                return answer
+            l.popleft()
             priorities.pop(0)
-            index.append(index[0])
-            index.pop(0)
-    return answer
+        else:
+            l.append(l.popleft())
