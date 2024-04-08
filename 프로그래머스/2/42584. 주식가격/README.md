a115cd733f1f87cd052ccdae552f7df9623b958b
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-04-08 14:36:50 +0900

    [level 2] Title: 주식가격, Time: 76.33 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#qna) 
 
 ### 성능 요약
 
-메모리: 17 MB, 시간: 75.08 ms
+메모리: 17 MB, 시간: 76.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 0:15:6
+2024년 04월 08일 14:36:47
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,12 +1,14 @@
 def solution(prices):
     answer = []
-    for i in range(len(prices)):
-        total = -1
-        for j in range(i,len(prices)):
+    n = len(prices)
+    for i in range(n):
+        count = -1
+        for j in range(i,n):
             if prices[i] <= prices[j]:
-                total +=1
+                count +=1
             else:
-                total +=1
+                count +=1
                 break
-        answer.append(total)
+        answer.append(count)
     return answer
+
