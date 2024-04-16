26ef67efc0c1d938defd5faef5ff4d6b03d98e0e
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-04-16 14:17:55 +0900

    [level 2] Title: 타겟 넘버, Time: 2.33 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,13 +1,11 @@
 def solution(numbers, target):
     answer = 0
-    leaves = [0]
-    for num in numbers:
+    result = [0]
+    for i in numbers:
         tmp = []
-        for parent in leaves:
-            tmp.append(parent + num)
-            tmp.append(parent - num)
-        leaves = tmp
-    for leaf in leaves:
-        if leaf == target:
-            answer += 1
+        for j in result:
+            tmp.append(j+i)
+            tmp.append(j-i)
+        result = tmp
+    answer = result.count(target)
     return answer
