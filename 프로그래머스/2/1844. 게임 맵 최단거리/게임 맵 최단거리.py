cbd9833279aa605eef83f7b8ff6ef379d084204b
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-04-17 18:28:06 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.89 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,14 +1,14 @@
 from collections import deque
 def solution(maps):
     answer = 0
-    n = len(maps[0]) #열
-    m = len(maps) #행
+    n = len(maps)
+    m = len(maps[0])
     
     dx = [1,-1,0,0]
     dy = [0,0,1,-1]
     
     INF = 1e9
-    dist = [[INF for _ in range(n)] for _ in range(m)]
+    dist = [[INF for _ in range(m)]for _ in range(n)]
     
     q = deque()
     q.append((0,0))
@@ -18,14 +18,12 @@ def solution(maps):
         for i in range(4):
             nx = a + dx[i]
             ny = b + dy[i]
-            if 0<=nx<n and 0<=ny<m and maps[ny][nx] == 1:
-                if dist[ny][nx] > dist[b][a] + 1:
-                    dist[ny][nx] = dist[b][a] + 1
+            if 0<=nx<n and 0<=ny<m and maps[nx][ny] == 1:
+                if dist[nx][ny] > dist[a][b] +1:
+                    dist[nx][ny] = dist[a][b] +1
                     q.append((nx,ny))
-    if dist[m-1][n-1] == INF:
+    if dist[n-1][m-1] == INF:
         answer = -1
     else:
-        answer = dist[m-1][n-1]
+        answer = dist[n-1][m-1]
     return answer
-
-    
