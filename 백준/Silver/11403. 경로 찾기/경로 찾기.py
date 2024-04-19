365d98359d95c9102c0931d1513dce67e1a111af
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-04-19 18:37:12 +0900

    [Silver I] Title: 경로 찾기, Time: 76 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,29 +1,29 @@
 from collections import deque
 N = int(input())
-l = [list(map(int,input().split())) for _ in range(N)]
-answer = []
+m = [list(map(int,input().split())) for _ in range(N)]
 graph = [[] for _ in range(N)]
 for i in range(N):
   for j in range(N):
-    if l[i][j] == 1:
+    if m[i][j] == 1:
       graph[i].append(j)
 
-def bfs(n):
+def bfs(n,visited):
   q = deque()
   q.append(n)
   while q:
     x = q.popleft()
     for i in graph[x]:
       if not visited[i]:
-        q.append(i)
         visited[i] = 1
+        q.append(i)
 
+answer = []
 for i in range(N):
   visited = [0] * N
-  bfs(i)
+  bfs(i,visited)
   answer.append(visited)
 
 for i in range(N):
   for j in range(N):
-    print(answer[i][j], end = " ")
+    print(answer[i][j],end=" ")
   print()
