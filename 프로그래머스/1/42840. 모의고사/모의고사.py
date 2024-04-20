9026ddc1013780479c5c879a56d2f6fd89e2b24c
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-04-20 19:41:50 +0900

    [level 1] Title: 모의고사, Time: 2.09 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,30 +1,22 @@
 def solution(answers):
     answer = []
-    l = []
-    n1 = [1,2,3,4,5]
-    n2 = [2,1,2,3,2,4,2,5]
-    n3 = [3,3,1,1,2,2,4,4,5,5]
-    l.append(func(n1,answers))
-    l.append(func(n2,answers))
-    l.append(func(n3,answers))
-    max_data = max(l)
-    for i in range(len(l)):
-        if l[i] == max_data:
+    l = len(answers)
+    a = [1,2,3,4,5] * (l//5+1)
+    b = [2,1,2,3,2,4,2,5] * (l//8+1)
+    c = [3,3,1,1,2,2,4,4,5,5] * (l//10+1)
+    a_c = 0
+    b_c = 0
+    c_c = 0
+    for i in range(l):
+        if a[i] == answers[i]:
+            a_c +=1
+        if b[i] == answers[i]:
+            b_c +=1
+        if c[i] == answers[i]:
+            c_c +=1
+    result = [a_c,b_c,c_c]
+    max_data = max(result)
+    for i in range(3):
+        if result[i] == max_data:
             answer.append(i+1)
     return answer
-
-def func(n,answers):
-    total = 0
-    if len(n) > len(answers):
-        for i in range(len(answers)):
-            if answers[i] == n[i]:
-                total +=1
-        return total
-    else: 
-        m = len(answers)//len(n)
-        n = n*(m+1)
-        for k in range(len(answers)):
-            if answers[k] == n[k]:
-                total +=1
-        return total
-            
