bdf8d6f124cbcb6a1f9e2877f81df0131c34bd73
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-04-23 00:34:46 +0900

    [Silver I] Title: 오목, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.py
+++ 백준/Silver/2615. 오목/오목.py
@@ -1,64 +1,25 @@
-def a(i,j,start,graph):
-  for _ in range(4):
-    if 0<=j+1<19 and graph[i][j+1] == start:
-      j +=1
-    else:
-      return False
-  if 0<=j+1<19 and graph[i][j+1] == start:
-    return False
-  if 0<=j-5<19 and graph[i][j-5] == start:
-    return False
-  else:
-    return True
-
-def b(i,j,start,graph):
-  for _ in range(4):
-    if 0<=i+1<19 and graph[i+1][j] == start:
-      i +=1
-    else:
-      return False
-  if 0<=i+1<19 and graph[i+1][j] == start:
-    return False
-  if 0<=i-5<19 and graph[i-5][j] == start:
-    return False
-  else:
-    return True
-
-def c(i,j,start,graph):
-  for _ in range(4):
-    if 0<=i+1<19 and 0<=j+1<19 and graph[i+1][j+1] == start:
-      j +=1
-      i +=1
-    else:
-      return False
-  if 0<=i+1<19 and 0<=j+1<19 and graph[i+1][j+1] == start:
-    return False
-  if 0<=i-5<19 and 0<=j-5<19 and graph[i-5][j-5] == start:
-    return False
-  else:
-    return True
-
-def d(i,j,start,graph):
-  for _ in range(4):
-    if 0<=i-1<19 and 0<=j+1<19 and graph[i-1][j+1] == start:
-      j +=1
-      i -=1
-    else:
-      return False
-  if 0<=i-1<19 and 0<=j+1<19 and graph[i-1][j+1] == start:
-    return False
-  if 0<=i+5<19 and 0<=j-5<19 and graph[i+5][j-5] == start:
-    return False
-  else:
-    return True
+dx = [1,0,1,1]
+dy = [0,1,1,-1]
 
 graph = [list(map(int,input().split())) for _ in range(19)]
 for i in range(19):
   for j in range(19):
     start = graph[i][j]
     if start != 0:
-      if a(i,j,start,graph) or b(i,j,start,graph) or c(i,j,start,graph) or d(i,j,start,graph):
-        print(start)
-        print(i+1,j+1)
-        exit()
+        for k in range(4):
+            cnt = 1
+            nx = j + dx[k]
+            ny = i + dy[k]
+            while 0<=nx<19 and 0<=ny<19 and graph[ny][nx] == start:
+                cnt +=1
+                if cnt == 5:
+                    if 0<=nx+dx[k]<19 and 0<=ny+dy[k]<19 and graph[ny+dy[k]][nx+dx[k]] == start:
+                        break
+                    if 0<=i-dy[k]<19 and 0<=j-dx[k]<19 and graph[i-dy[k]][j-dx[k]] == start:
+                        break
+                    print(start)
+                    print(i+1,j+1)
+                    exit()
+                nx += dx[k]
+                ny += dy[k]
 print(0)
