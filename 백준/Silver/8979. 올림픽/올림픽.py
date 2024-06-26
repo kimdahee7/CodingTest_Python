f01685727b1b5b494298ead47a14886f6bddf869
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-06-26 12:36:07 +0900

    [Silver V] Title: 올림픽, Time: 48 ms, Memory: 31252 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -7,14 +7,17 @@ l.sort(key=lambda x:-x[2])
 l.sort(key=lambda x:-x[1])
 answer_list = [(l[0][0],1)]
 answer = 1
+count = 1
 g = l[0][1]
 s = l[0][2]
 b = l[0][3]
 for i in range(1,N):
     if l[i][1] == g and l[i][2] == s and l[i][3] == b:
         answer_list.append((l[i][0],answer))
+        count +=1
     else:
-        answer +=1
+        answer = count +1
+        count +=1
         g = l[i][1]
         s = l[i][2]
         b = l[i][3]
