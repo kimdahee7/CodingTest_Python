e75a143ece4a48e861d6352f08c32d4209fbace7
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-10-16 02:42:28 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -1,20 +1,21 @@
 N = int(input())
-map = [list(map(int,input().split())) for _ in range(N)]
-dp = [[[0 for _ in range(3)] for _ in range(N)] for _ in range(N)]
+pipe = [list(map(int,input().split())) for _ in range(N)]
+dp = [[[0] * 3 for _ in range(N)] for _ in range(N)]
 
-# 가로 0 / 세로 1 / 대각선 2
-# 0행은 가로로만 이동 가능
-dp[0][1][0] = 1
-for i in range(2,N):
-    if map[0][i] == 0:
-        dp[0][i][0] = dp[0][i-1][0]
+#0-가로, 1-대각선, 2-아래
+for i in range(N):
+    for j in range(1,N):
+        if i == 0 and pipe[i][j] == 0:
+            dp[i][j][0] = 1
+        else:
+            break
 
 for i in range(1,N):
     for j in range(1,N):
-        if map[i][j] == 0 and map[i-1][j] == 0 and map[i][j-1] == 0:
-            dp[i][j][2] = dp[i-1][j-1][1] + dp[i-1][j-1][0] + dp[i-1][j-1][2]
-        if map[i][j] == 0:
-            dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2]
-            dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2]
+        if pipe[i][j] == 0:
+            dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][1]
+            dp[i][j][2] = dp[i-1][j][1] + dp[i-1][j][2]
+        if pipe[i][j] == 0 and pipe[i-1][j] == 0 and pipe[i][j-1] == 0:
+            dp[i][j][1] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][2] + dp[i - 1][j - 1][1]
 
 print(sum(dp[N-1][N-1]))
