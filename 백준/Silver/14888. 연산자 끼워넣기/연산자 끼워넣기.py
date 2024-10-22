5b8c2f0142858beb6dcae9e2d3eec77c0e0115cc
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-10-22 23:14:20 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,34 +1,35 @@
-from itertools import permutations
-
 N = int(input())
-A = list(map(int,input().split()))
-l = list(map(int,input().split()))
-total_cal = []
-cal = ["+","-","x","/"]
+A = list(map(int, input().split()))
+l = list(map(int, input().split()))
+cal = ["+", "-", "x", "/"]
 answer = []
 
-for i in range(4):
-    if l[i] != 0:
-        for j in range(l[i]):
-            total_cal.append(cal[i])
 
-permu = permutations(total_cal,N-1)
-for p in permu:
-    result = A[0]
-    for i in range(1,N):
-        if p[i-1] == "+":
-            result += A[i]
-        elif p[i-1] == "-":
-            result -= A[i]
-        elif p[i-1] == "x":
-            result *= A[i]
-        else:
-            if result < 0:
-                result = -result//A[i]
-                result = -result
+def backtracking(result, l, index):
+    if index == N:
+        answer.append(result)
+        return
+    tmp = result
+    for i in range(4):
+        if l[i] > 0:
+            l[i] -= 1
+            if i == 0:
+                result += A[index]
+            elif i == 1:
+                result -= A[index]
+            elif i == 2:
+                result *= A[index]
             else:
-                result //= A[i]
-    answer.append(result)
+                if result < 0:
+                    result = -result // A[index]
+                    result = -result
+                else:
+                    result //= A[index]
+            backtracking(result, l, index + 1)
+            l[i] += 1
+            result = tmp
+
+backtracking(A[0], l, 1)
 answer.sort()
 print(answer[-1])
 print(answer[0])
