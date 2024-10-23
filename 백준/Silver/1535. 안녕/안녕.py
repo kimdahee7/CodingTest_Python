df0971b4ddf786a8e81f5c605dbe70ad2c6c603d
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-10-23 20:17:52 +0900

    [Silver II] Title: 안녕, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.py
+++ 백준/Silver/1535. 안녕/안녕.py
@@ -1,23 +1,15 @@
-from itertools import combinations
-
 N = int(input())
 power = list(map(int,input().split()))
 happy = list(map(int,input().split()))
-l = [i for i in range(N)]
-answer = []
+power = [0] + power
+happy = [0] + happy
+dp = [[0 for _ in range(101)] for _ in range(N+1)]
 
 for i in range(1,N+1):
-    combi = combinations(l,i)
-    for c in combi:
-        total_power = 0
-        total_happy = 0
-        for j in c:
-            total_power+=power[j]
-            total_happy+=happy[j]
-        if total_power < 100:
-            answer.append(total_happy)
-answer.sort()
-if len(answer) == 0:
-    print(0)
-else:
-    print(answer[-1])
+    for j in range(1,101):
+        # 뺄 수 있는 경우
+        if power[i] <= j:
+            dp[i][j] = max(dp[i-1][j],dp[i-1][j-power[i]]+happy[i])
+        else:
+            dp[i][j] = dp[i-1][j]
+print(dp[N][99])
