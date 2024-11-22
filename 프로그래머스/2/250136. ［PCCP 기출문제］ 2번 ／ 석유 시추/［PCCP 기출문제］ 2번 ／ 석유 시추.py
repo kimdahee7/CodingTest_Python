fcec20da853ad38f158d19aa6fa5cb6132f8079d
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-11-23 01:21:28 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 351.41 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -2,37 +2,46 @@ from collections import deque
 
 def solution(land):
     answer = 0
-    M = len(land[0])
-    N = len(land)
-    dp = [0] * (M+1)
-    for i in range(N):
-        for j in range(M):
-            if land[i][j] == 1:
-                bfs(j,i,land,dp,M,N)
-    answer = max(dp)
+    X = len(land[0])
+    Y = len(land)
+    pipe = []
+    visited = [[0 for i in range(X)] for _ in range(Y)]
+    count = 1
+    for x in range(X):
+        total = 0
+        check = set()
+        for y in range(Y):
+            if land[y][x] == 1 and visited[y][x] == 0:
+                result = bfs(x,y,X,Y,count,land,visited)
+                total += result
+                pipe.append(result)
+                check.add(count)
+                count +=1
+            elif land[y][x] == 1 and visited[y][x] != 0 and visited[y][x] not in check:
+                total += pipe[visited[y][x]-1]
+                check.add(visited[y][x])
+                
+        answer = max(answer,total)
     return answer
+
 
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
-
-def bfs(x,y,land,dp,M,N):
-    l = set()
+def bfs(x,y,X,Y,count,land,visited):
+    cnt = 1
     q = deque()
     q.append((x,y))
-    land[y][x] = 0
-    l.add(x)
-    total = 1
+    visited[y][x] = count
     while q:
         a,b = q.popleft()
         for i in range(4):
-            nx = a + dx[i]
-            ny = b + dy[i]
-            if 0<=nx<M and 0<=ny<N and land[ny][nx] == 1:
-                total +=1
+            nx = dx[i] + a
+            ny = dy[i] + b
+            if 0<=nx<X and 0<=ny<Y and visited[ny][nx] == 0 and land[ny][nx] == 1:
                 q.append((nx,ny))
-                l.add(nx)
-                land[ny][nx] = 0
-    for k in l:
-        dp[k] += total
+                visited[ny][nx] = count
+                cnt +=1
+    return cnt
+                
     
-        
+    
