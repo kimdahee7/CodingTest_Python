68c8e3a1ddc91ed787e376e13b84d9f024d35da7
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2024-11-27 12:26:10 +0900

    [Gold V] Title: 선 긋기, Time: 2984 ms, Memory: 172204 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.py
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -1,25 +1,24 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 l = []
 for _ in range(N):
     x, y = map(int, input().split())
     l.append((x, y))
 
-l.sort(key=lambda x: (x[0], -x[1]))
+l.sort()
 start = l[0][0]
 end = l[0][1]
-total = end - start
+total = 0
 for i in range(1, N):
-    # 이전 범위에 포함되어 있을 경우
-    if start <= l[i][0] and l[i][1] <= end:
-        continue
+    # 겹쳐있을 경우
+    if l[i][0] <= end:
+        end = max(end,l[i][1])
     # 시작점이 이전 범위보다 클 경우
-    elif end < l[i][0]:
-        total += l[i][1] - l[i][0]
+    else:
+        total += end - start
         start = l[i][0]
         end = l[i][1]
-    # 범위가 겹쳐있을 경우
-    elif start <= l[i][0] and l[i][1] > end:
-        total += l[i][1] - end
-        end = l[i][1]
-
+total += end-start
 print(total)
