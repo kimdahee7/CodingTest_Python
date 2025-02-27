ecb6e0d6aec4d0f915e8ac6d8ac785c57d74a2fd
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2025-02-27 21:33:14 +0900

    [Silver II] Title: 예산, Time: 1332 ms, Memory: 111004 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,32 +1,28 @@
-import sys
-input = sys.stdin.readline
-def main():
-  N = int(input())
-  l = list(map(int,input().split()))
-  l.sort()
-  max_data = max(l)
-  M = int(input())
-  if sum(l) <= M:
-    print(max_data)
-  else:
-    print(binary(l,max_data,M))
+N = int(input())
+N_list = list(map(int,input().split()))
+M = int(input())
 
-def binary(l,max_data,M):
-  start = 0
-  end = max_data
-  r= 0
-  while start <= end:
+N_list.sort()
+start = 0
+end = N_list[-1]
+result = 0
+answer = 0
+while start <= end:
+    mid = (start + end) // 2
     total = 0
-    mid = (start+end) //2 
-    for i in l:
-      if i <=mid:
-        total += i
-      else:
-        total +=mid
-    if total <= M:
-      start = mid+1
-      r = mid
+    for i in N_list:
+        if i <= mid:
+            total += i
+        else:
+            total += mid
+    if total > M:
+        end -=1
+    elif total < M:
+        if result < total:
+            result = total
+            answer = mid
+        start +=1
     else:
-      end = mid-1
-  return r
-main()
+        print(mid)
+        exit()
+print(answer)
