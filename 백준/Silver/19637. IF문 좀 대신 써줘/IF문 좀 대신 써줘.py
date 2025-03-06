dfa8fccd7a61b39d7284dcabc8a0c949a7647752
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2025-03-06 15:11:46 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 676 ms, Memory: 44216 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
@@ -1,25 +1,23 @@
 import sys
 input = sys.stdin.readline
-
 N,M = map(int,input().split())
 level = []
-for i in range(N):
-    a,b = map(str,input().split())
-    level.append((a,int(b)))
-
-def binary(level,k):
-    answer = ""
-    start = 0
-    end = len(level)-1
-    while start<=end:
-        mid = (start + end)//2
-        if k <= level[mid][1]:
-            answer = level[mid][0]
-            end = mid - 1
-        else:
-            start = mid +1
-    return answer
-
-for i in range(M):
-    k = int(input())
-    print(binary(level,k))
+score = []
+for _ in range(N):
+  a,b = map(str,input().split())
+  level.append(a)
+  score.append(int(b))
+  
+for _ in range(M):
+  s = int(input())
+  start = 0
+  end = N-1
+  answer = ""
+  while start <= end:
+    mid = (start+end)//2
+    if score[mid] >= s:
+      answer = level[mid]
+      end = mid - 1
+    else:
+      start = mid + 1
+  print(answer)
