aa3ff3e6a6322adcd370fb7fc4c105641741d760
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2025-03-11 21:08:02 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 56 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -1,13 +1,10 @@
 T = int(input())
-dp = [0] * 10001
-dp[1] = 1
-dp[2] = 2
-dp[3] = 3
+dp = [0]* 10001
+dp[1] =1
+dp[2] =2
+dp[3] =3
+for i in range(4,10001):
+  dp[i] = dp[i-3] + i//2 + 1
 for _ in range(T):
   n = int(input())
-  if n <= 3:
-    print(dp[n])
-  else:
-    for i in range(4,n+1):
-      dp[i] = dp[i-3] + (i//2) +1
-    print(dp[n])
+  print(dp[n])
