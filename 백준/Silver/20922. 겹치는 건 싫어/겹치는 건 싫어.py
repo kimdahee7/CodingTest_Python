da2dd5a7bd58e2fba084f22e4c5132bab375a8c2
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2025-03-12 19:35:11 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 340 ms, Memory: 53288 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -1,22 +1,23 @@
 N,K = map(int,input().split())
-l = list(map(int,input().split()))
+a = list(map(int,input().split()))
 dic = {}
-for i in l:
+for i in a:
     if i not in dic:
         dic[i] = 0
 
 start = 0
-end = 0
+end = 1
+tmp = 1
 answer = 1
-total = 0
-dic[l[start]] +=1
-while start < N:
-    if end + 1 < N and dic[l[end+1]] + 1 <= K:
-        end += 1
-        dic[l[end]] += 1
-        answer = max(answer,end-start+1)
+dic[a[0]] = 1
+while start <= end < N:
+    if (dic[a[end]] + 1) <= K:
+        dic[a[end]] +=1
+        end +=1
+        tmp +=1
     else:
-        dic[l[start]] -=1
+        dic[a[start]] -=1
         start +=1
-
+        tmp -=1
+    answer = max(answer,tmp)
 print(answer)
