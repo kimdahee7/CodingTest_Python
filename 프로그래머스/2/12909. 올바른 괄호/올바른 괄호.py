200996b1baba9672a1f33571e0abc4cd3d42be90
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2025-03-18 23:30:27 +0900

    [level 2] Title: 올바른 괄호, Time: 13.71 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -2,11 +2,14 @@ def solution(s):
     answer = True
     stack = []
     for i in s:
-        if i == ")" and len(stack) != 0 and stack[-1] == "(":
+        if len(stack) == 0:
+            stack.append(i)
+        elif i == ")" and stack[-1] == "(":
             stack.pop()
         else:
             stack.append(i)
     if len(stack) == 0:
-        return True
+        answer = True
     else:
-        return False
+        answer = False
+    return answer
