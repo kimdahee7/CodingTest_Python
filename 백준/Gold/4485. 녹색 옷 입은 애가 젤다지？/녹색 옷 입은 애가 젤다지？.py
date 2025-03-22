79be70172d0b171177ec72aa61c2d101698fb987
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2025-03-22 23:57:24 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 124 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,25 +1,30 @@
-from collections import deque
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-INF = 1e9
-c = 0
+import sys
+input = sys.stdin.readline
+import heapq
+
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+num = 0
 while True:
-  c +=1
-  N = int(input())
-  if N == 0:
-    break
-  graph = [list(map(int,input().split())) for _ in range(N)]
-  dist = [[INF for _ in range(N)] for _ in range(N)]
-  q = deque()
-  q.append((0,0))
-  dist[0][0] = graph[0][0]
-  while q:
-    a,b = q.popleft()
-    for i in range(4):
-      nx = a + dx[i]
-      ny = b + dy[i]
-      if 0<=nx<N and 0<=ny<N:
-        if dist[ny][nx] > dist[b][a] + graph[ny][nx]:
-          dist[ny][nx] = dist[b][a] + graph[ny][nx]
-          q.append((nx,ny))
-  print("Problem "+str(c)+": "+str(dist[N-1][N-1]))
+    num += 1
+    N = int(input())
+    if N == 0:
+        exit()
+    graph = [list(map(int,input().split())) for _ in range(N)]
+    dist = [[1e9 for _ in range(N)] for _ in range(N)]
+    h = []
+    heapq.heappush(h,(graph[0][0],0,0))
+    dist[0][0] = graph[0][0]
+    while h:
+        r,x,y = heapq.heappop(h)
+        if y == N-1 and x == N-1:
+            print("Problem " + str(num) + ": " + str(r) )
+            break
+        if dist[y][x] < r:
+            continue
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+            if 0<=nx<N and 0<=ny<N and dist[ny][nx] > dist[y][x] + graph[ny][nx]:
+                dist[ny][nx] = dist[y][x] + graph[ny][nx]
+                heapq.heappush(h,(dist[ny][nx],nx,ny))
