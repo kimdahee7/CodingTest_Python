49812ad33e95673aae3b308b526b3dbd0c52a53b
Kimdahee <62695395+kimdahee7@users.noreply.github.com>
2025-04-16 23:17:43 +0900

    [Gold V] Title: 택배 배송, Time: 1476 ms, Memory: 52328 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.py
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -1,22 +1,26 @@
 import heapq
 
-N,M = map(int,input().split())
-INF = 1e9
-graph = [[] for _ in range(N+1)]
-dist = [INF] * (N+1)
+def dijk(start):
+    h = []
+    heapq.heappush(h,(0,start))
+    dist[start] = 0
+    while h:
+        d,now = heapq.heappop(h)
+        if dist[now] < d:
+            continue
+        for i in route[now]:
+            cost = d + i[1]
+            if cost < dist[i[0]]:
+                dist[i[0]] = cost
+                heapq.heappush(h,(cost,i[0]))
 
-for i in range(M):
-  a,b,c = map(int,input().split())
-  graph[a].append((b,c))
-  graph[b].append((a,c))
-  
-q = []
-heapq.heappush(q,(1,0))
-dist[1] = 0
-while q:
-  p,t = heapq.heappop(q)
-  for i in graph[p]:
-    if dist[i[0]] > dist[p] + i[1]:
-      dist[i[0]] = dist[p] + i[1]
-      heapq.heappush(q,(i[0],i[1]))
+N,M = map(int,input().split())
+route = [[] for _ in range(N+1)]
+dist = [1e9 for _ in range(N+1)]
+for _ in range(M):
+    a,b,c = map(int,input().split())
+    route[a].append((b,c))
+    route[b].append((a,c))
+
+dijk(1)
 print(dist[N])
